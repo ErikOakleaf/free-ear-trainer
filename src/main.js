e9497007b3abdcc2b109d0a4a3cb1bbe64dbd076
ErikOakleaf <oakl33f123@gmail.com>
2025-01-26 20:33:45 +0100

    added getting new interval on correct button

--- src/main.js
+++ src/main.js
@@ -3,65 +3,77 @@ const { invoke } = window.__TAURI__.core;
 let currentInterval = [];
 const intervalButtons = document.querySelectorAll(".interval-button");
 
-document.getElementById("testButton").addEventListener("click", () => {
-    invoke("get_interval", { validIntervals: 8191 }).then((interval) => {
-        currentInterval = interval;
-        console.log(currentInterval);
-        invoke("play_interval_audio", {
-            intervalInSemitones: currentInterval,
-        });
+async function get_new_interval() {
+    currentInterval = await invoke("get_interval", { validIntervals: 8191 });
+}
+
+function play_interval_audio() {
+    invoke("play_interval_audio", {
+        intervalInSemitones: currentInterval,
     });
+}
+
+document.getElementById("testButton").addEventListener("click", () => {
+    play_interval_audio();
 });
+
+async function handleButtonClick(event) {
+    // button id will represnt the interval as a string
+    const buttonId = event.target.id;
+
+    // converting string to int that represents the interval in semitones
+
+    const intervalToSemitones = {
+        unison: 0,
+        minorSecond: 1,
+        majorSecond: 2,
+        minorThird: 3,
+        majorThird: 4,
+        perfectFourth: 5,
+        tritone: 6,
+        perfectFifth: 7,
+        minorSixth: 8,
+        majorSixth: 9,
+        minorSeventh: 10,
+        majorSeventh: 11,
+        perfectOctave: 12,
+    };
+
+    const semitones = intervalToSemitones[buttonId];
+
+    const currentInterval_semitones = currentInterval[1] - currentInterval[0];
+
+    console.log(semitones);
+    console.log(currentInterval_semitones);
+
+    if (currentInterval_semitones == semitones) {
+        // turn every button to white if the correct answer is given
+
+        intervalButtons.forEach((button) => {
+            button.style.backgroundColor = "white";
+        });
+
+        // blink the current button green then white on the correct button
+        const currentButton = document.getElementById(buttonId);
+        currentButton.style.backgroundColor = "#50C878";
+        setTimeout(() => {
+            currentButton.style.backgroundColor = "white";
+        }, 300);
+
+        // get a new interval
+        await get_new_interval();
+        play_interval_audio();
+    } else {
+        const currentButton = document.getElementById(buttonId);
+        currentButton.style.backgroundColor = "#CD5C5C";
+    }
+}
 
 intervalButtons.forEach((button) => {
-    button.addEventListener("click", (event) => {
-        // button id will represnt the interval as a string
-        const buttonId = event.target.id;
-
-        // converting string to int that represents the interval in semitones
-
-        const intervalToSemitones = {
-            unison: 0,
-            minorSecond: 1,
-            majorSecond: 2,
-            minorThird: 3,
-            majorThird: 4,
-            perfectFourth: 5,
-            tritone: 6,
-            perfectFifth: 7,
-            minorSixth: 8,
-            majorSixth: 9,
-            minorSeventh: 10,
-            majorSeventh: 11,
-            perfectOctave: 12,
-        };
-
-        const semitones = intervalToSemitones[buttonId];
-
-        const currentInterval_semitones =
-            currentInterval[1] - currentInterval[0];
-
-        console.log(semitones);
-        console.log(currentInterval_semitones);
-
-        if (currentInterval_semitones == semitones) {
-            // turn every button to white if the correct answer is given
-
-            intervalButtons.forEach((button) => {
-                button.style.backgroundColor = "white";
-            });
-
-            // blink the current button green then white on the correct button
-            const currentButton = document.getElementById(buttonId);
-            currentButton.style.backgroundColor = "#50C878";
-            setTimeout(() => {
-                currentButton.style.backgroundColor = "white";
-            }, 300);
-        } else {
-            const currentButton = document.getElementById(buttonId);
-            currentButton.style.backgroundColor = "#CD5C5C";
-        }
-    });
+    button.addEventListener("click", handleButtonClick);
 });
 
-window.addEventListener("DOMContentLoaded", () => { });
+window.addEventListener("DOMContentLoaded", async () => {
+    await get_new_interval();
+    play_interval_audio();
+});
